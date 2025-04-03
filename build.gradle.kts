fc7fa862f2f13a11a6daa29765b5e20f187f816f
Máté Molnár <matemolnar@protonmail.com>
2025-04-03 20:58:46 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+}
+
+group = "com.github.moln-r"
+version = "1.0"
+
+application {
+    mainClass.set("com.github.molnr.MainKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
